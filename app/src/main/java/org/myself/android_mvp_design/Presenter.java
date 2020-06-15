1b352311e24ba31ff348b4d9cefe064776f9cf77
Razaul Munir <razaulmunir@gmail.com>
2020-06-15 13:03:30 +0600

    presenter is ready

--- app/src/main/java/org/myself/android_mvp_design/Presenter.java
+++ app/src/main/java/org/myself/android_mvp_design/Presenter.java
@@ -1,4 +1,33 @@
 package org.myself.android_mvp_design;
 
 public class Presenter {
+
+    Model model;
+    PresenterListener listener;
+
+    public Presenter(PresenterListener listener) {
+        this.model = new Model();
+        this.listener = listener;
+    }
+
+    public void updateName(String name){
+        model.setName(name);
+        listener.getModelFullInfo(model.getFullInfo());
+    }
+
+    public void updateEmail(String email){
+        model.setEmail(email);
+        listener.getModelFullInfo(model.getFullInfo());
+    }
+
+    public void updateAddress(String address){
+        model.setAddress(address);
+        listener.getModelFullInfo(model.getFullInfo());
+    }
+
+    public interface PresenterListener{
+        void getModelFullInfo(String info);
+        void showProgressBar();
+        void hideProgressBar();
+    }
 }
